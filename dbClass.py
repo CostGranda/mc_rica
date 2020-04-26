7197201cf9209ccf1f15a8242d205535455ecb4d
Fabio Sebastian Montoya Arrubla <fsebas-m@hotmail.com>
2020-04-25 22:08:32 -0500

    InsertOne

--- dbClass.py
+++ dbClass.py
@@ -8,16 +8,22 @@ class DbConection():
         self.client = MongoClient("mongodb+srv://RICA:"+password+"@cluster0-hgvcm.mongodb.net/test?retryWrites=true&w=majority")
         self.db = self.client.get_database(database)
 
-#   Prueba de coneccion a base de datos, lectura de datos
-    def Prueba(self):
-        records = self.db.applicants
+#   Obtiene todos los documentos de una coleccion dada
+    def getAll(self, collection):
+        records = self.db[collection]
         print(list(records.find()))
 
 #   Insercion de multiples datos a base de datos
-    def insert(self, collect, jsonlist):
-        coll = self.db[collect]
+    def insertMany(self, collection, jsonlist):
+        coll = self.db[collection]
         inserts = coll.insert_many(jsonlist)
         print(inserts) 
+
+#   Insertar un registro
+    def insertOne(self, collection, json ):
+        coll = self.db[collection]
+        insert = coll.insert_one(json)
+        print(insert)
         
 
 #instance2 = DbConection('test','IammasterAlfa5')
@@ -70,11 +76,37 @@ json = [{
 }
 ]
 
-collect = 'applicants'
+jsonOne = {
+    "telefonos": [
+        3023001558
+    ],
+    "especialidades": [
+        "ABAP-HANA"
+    ],
+    "comentarios": [
+        "Es un buen muchacho"
+    ],
+    "cedula": 1036670456,
+    "nombres": "Fabio Sebastian",
+    "apellidos": "Montoya Arrubla",
+    "ciudad": "Medellín",
+    "email": "fabio.montoya@perceptio.net",
+    "disponibilidad": "2020-11-18",
+    "cv": "http://ria-env-2.vvpamseyk3.sa-east-1.elasticbeanstalk.com/api/cv/1017242314.pdf/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBZG1pbiIsImlhdCI6OTk5OTk5OTk5OSwiZXhwIjo5OTk5OTk5OTk5fQ.QbQCFQNCnf43SMztVVk6HeRPXvp1jun4hfUx5kBZzfI",
+    "calificacion": 5,
+    "state": "En proceso",
+    "origen": "Manual",
+    "content": ""
+}
+
+collection = 'applicants'
+
 #instanciacion de objetos dao y prueba de funcionamiento
 instance = DbConection('RicaDb','*Perceptio2020')
 #Lectura a lista de aplications
-instance.Prueba()
+instance.getAll(collection)
 #Insercion a colection a base de datos
-instance.insert(collect, json)
+#instance.insertMany(collect, json)
+#Insertar un registro
+#instance.insertOne(collection, jsonOne)
 
